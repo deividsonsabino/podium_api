3657cb1fc5110464953981df953cb139f3bb7539
Deivao <xdeividsabinox@live.com>
2026-01-30 16:17:21 -0300

    Update Jason dependency version and reorder dependencies in mix.exs

--- mix.exs
+++ mix.exs
@@ -49,9 +49,10 @@ defmodule PodiumApi.MixProject do
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 1.0"},
-      {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:httpoison, "~> 2.0"},
+      {:jason, "~> 1.4"}
     ]
   end
 
